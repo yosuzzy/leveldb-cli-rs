cd087a00347ab1a37b61d178e2551bb722a90073
yosuzzy <petere123123@gmail.com>
2024-05-17 17:03:03 +0900

    chore: bug

--- src/main.rs
+++ src/main.rs
@@ -123,8 +123,11 @@ fn main() {
         }
 
         let hash: &[u8] = key.as_bytes();
+        println!("hash: {:?}",hash);
+        let hash_key = &BlockRow::meta_key(full_hash(&hash[..]));
+        println!("hash_key: {:?}",hash);
         // 값 저장
-        let value: Option<BlockMeta>  = db.get(&BlockRow::meta_key(full_hash(&hash[..])))
+        let value: Option<BlockMeta>  = db.get(hash_key)
             .map(|val| deserialize_little(&val).expect("failed to parse BlockMeta"));
         println!("Value '{:?}'", value);
     }
